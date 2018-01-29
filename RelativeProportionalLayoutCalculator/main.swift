5cfab05c19367fde617e6c3d668eaac7a0167e00
Ben Zeeman <ben@zaapp.group>
2018-01-29 16:59:23 -0700

    Updated Usage Description

--- RelativeProportionalLayoutCalculator/main.swift
+++ RelativeProportionalLayoutCalculator/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let usageDescription = "Calculate the {Multiplier} value the constraints in Interface Builder. \n\n This program assumes you have included the following constraints from the Subview to the Superview: \n\tCenter Horizontally\n\tCenter Vertically\n\tEqual Widths\n\tEqual Heights \n\n Usage: \n\tQuick Calculation: rplc [SuperView Width] [SuperView Height] [View X] [View Y] [View Width] [View Height] \n\n Multiple Calculations: rplc [SuperView Width] [SuperView Height] \n\tthen:\n\t  [View X] [View Y] [View Width] [View Height] or Press Enter to Exit\n"
+let usageDescription = "Calculate the {Multiplier} value relative constraints in Interface Builder. \n\n This program assumes you have included the following constraints from the Subview to the Superview: \n\tCenter Horizontally\n\tCenter Vertically\n\tEqual Widths\n\tEqual Heights \n\n Usage: \n\tQuick Calculation: rplc [SuperView Width] [SuperView Height] [View X] [View Y] [View Width] [View Height] \n\n Multiple Calculations: rplc [SuperView Width] [SuperView Height] \n\tthen:\n\t  [View X] [View Y] [View Width] [View Height] or Press Enter to Exit\n"
 
 func writeToStdOut(str: String) {
     //print(str)
